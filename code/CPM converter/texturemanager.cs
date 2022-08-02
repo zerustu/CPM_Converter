8641c707f7219eb2d8757975f22ae5e050ab2d11
val9k <wasquel.valentin@gmail.com>
2022-08-02 17:23:28 +0200

    animation update part 3
    finaliza all animation variables and add visibility handle

---
+++ code/CPM converter/texturemanager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CPM_converter
+{
+    class texturemanager
+    {
+        Dictionary<string, (int, int, int)> texlist;
+        int nextoffset;
+
+        public texturemanager()
+        {
+            nextoffset = 0;
+            texlist = new Dictionary<string, (int, int, int)>();
+        }
+
+        public void addText(string texName, int res)
+        {
+            if (texlist.ContainsKey(texName)) return;
+            texlist.Add(texName, (nextoffset, res, res));
+        }
+
+        public int getOffset(string texName)
+        {
+            return texlist.ContainsKey(texName) ? texlist[texName].Item1 : throw new Exception("a texture appeard mid conversion. what?");
+        }
+    }
+}
